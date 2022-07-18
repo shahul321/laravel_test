f493c566097e67a285b4da6a35de577ce6701c61
Shahul Hameed <shahul.me@gmail.com>
2022-07-18 16:13:42 +0530

    getEventsWithWorkshops function

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
+class Event extends Model {
+
+    public function workshop() {
+        return $this->hasMany('App\Models\Workshop');
+    }
+
 }
