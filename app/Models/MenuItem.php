f83ed34ce29567d7c6a8500f25a09bb7ed72f1d3
Shahul Hameed <shahul.me@gmail.com>
2022-07-18 17:14:44 +0530

    menu

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -1,12 +1,18 @@
 <?php
 
-
 namespace App\Models;
-
 
 use Illuminate\Database\Eloquent\Model;
 
-class MenuItem extends Model
-{
+class MenuItem extends Model {
+
+    public function items() {
+        return $this->hasMany(MenuItem::class, 'parent_id');
+    }
+
+   
+    public function childItems() {
+        return $this->hasMany(MenuItem::class, 'parent_id')->with('items');
+    }
 
 }
